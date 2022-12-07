de598c2b029c13b1f5a9be94a28be4e22bf56ba1
Willian <willian.neibertz@gmail.com>
2022-12-07 18:50:23 -0300

    Implements win msg on webpage and successfuly adds a class to html outputBox

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const ChoicesList = ["rock", "papper", "scissors"];
 const buttons = document.querySelectorAll ('button');
+const outputBox = document.querySelector ('.outputBox');
+
+let round = 0;
+let cmpWin = 0;
+let plyrWin =0;
+
 
 buttons.forEach((button) => {
   button.addEventListener ('click',() => {
     playerChoice = button.id;
     let computerChoice = getComputerChoice();
-    checkWinner(playerChoice,computerChoice);
+    console.log(playerChoice + " vs " + computerChoice);
+    let text = checkWinner(playerChoice,computerChoice);
+    outputBox.textContent = text;
+    outputBox.classList.add('outputText');
   })
 });
 
@@ -13,20 +22,21 @@ buttons.forEach((button) => {
 
 function getComputerChoice() {
   randomPick = ChoicesList[Math.floor(Math.random()*ChoicesList.length)];
-  console.log(randomPick);
   return randomPick;
 }
 
 function checkWinner(player, computer) {
+  let gameMsg = "";
   if (player === "rock" && computer === "papper"){
-    alert(`You lose! You picked ${player} and I played ${computer}!`)
+    gameMsg = `You lose! You picked ${player} and I played ${computer}!`
   }else if (player === "papper" && computer === "scissors"){
-    alert(`You lose! You picked ${player} and I played ${computer}!`)
+    gameMsg = `You lose! You picked ${player} and I played ${computer}!`
   }else if (player === "scissors" && computer === "rock"){
-    alert(`You lose! You picked ${player} and I played ${computer}!`)
+    gameMsg =`You lose! You picked ${player} and I played ${computer}!`
   }else if (player === computer){
-    alert(`It's a Draw! We both picked ${player}!`)
+    gameMsg = `It's a Draw! We both picked ${player}!`
   }else {
-    alert(`You win, congratulations! \nYou picked ${player} and I played ${computer}!`)
+    gameMsg = `You win, congratulations! \nYou picked ${player} and I played ${computer}!`
   }
+  return gameMsg
 }
