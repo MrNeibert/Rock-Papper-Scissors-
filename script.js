19889931febd14a0342d5824de3ffe896515cb40
Willian <willian.neibertz@gmail.com>
2022-12-08 00:17:14 -0300

    player score implemented. initial attempt to implement a limit of 5 rounds

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 const ChoicesList = ["rock", "papper", "scissors"];
 const buttons = document.querySelectorAll ('button');
 const outputBox = document.querySelector ('.outputBox');
+const scorePlayer = document.getElementById ("scorePlayer");
+const scorePc = document.getElementById ("scorePc");
+const roundElement = document.getElementById ("round");
 
-let round = 0;
 let cmpWin = 0;
 let plyrWin =0;
+let roundsLeft = 5;
 
+buttons.forEach((button) => { 
+  button.addEventListener ('click',addEvent(button))
 
-buttons.forEach((button) => {          //buttons acts similarly as an array, but of nodes.
-  button.addEventListener ('click',() => {   //for each item (button) in buttons, linsten for clicks, and do:
-    playerChoice = button.id;    //asigns the button id to the players choice
+  });
+
+   
+function addEvent(button) {
+  return () => {
+    playerChoice = button.id; //asigns the button id to the players choice
     let computerChoice = getComputerChoice();
-    let text = checkWinner(playerChoice,computerChoice);
-    outputBox.textContent = text;
-    outputBox.classList.add('outputText'); //addsthe class whose css has background white
-  })
-});
-
-
+    outputBox.textContent = checkWinner(playerChoice, computerChoice); //runs logic and return game msg
+    outputBox.classList.add('outputText'); //adds the class whose css has background white
+    console.log(plyrWin + " vs " + cmpWin);
+    scorePc.innerHTML = `Computer: ${cmpWin}`;
+    scorePlayer.innerHTML = `Player: ${plyrWin}`;
+    roundElement.innerHTML = `Number of rounds left:${roundsLeft}`;
+    if (roundsLeft <= 0) {
+      disableButtons();
+    }
+  };
+}
 
 function getComputerChoice() {
   randomPick = ChoicesList[Math.floor(Math.random()*ChoicesList.length)]; // randommly chose a value between 0 and 1
@@ -30,10 +42,19 @@ function checkWinner(player, computer) {
       player == "papper" && computer == "scissors" ||
       player == "scissors" && computer == "rock"){
     gameMsg = `You lose! You picked ${player} and I played ${computer}!`
+    cmpWin ++;  // adds a point to computer
   }else if (player == computer){
     gameMsg = `It's a Draw! We both picked ${player}!`
   }else {
     gameMsg = `You win, congratulations! You picked ${player} and I played ${computer}!`
+    plyrWin ++; // adds a point to player
   }
+  roundsLeft --
   return gameMsg
+}
+
+function disableButtons(){
+  buttons.forEach ((button) => {
+    button.removeEventListener('click',addEvent);
+  })
 }
